1ab525584779b5f448c0ca41aa26ad4b7d8b93b9
Haichuan23 <haichuanwang23@gmail.com>
2022-04-08 15:37:05 -0500

    9b

--- HW1_9_MyBernoulli.R
+++ HW1_9_MyBernoulli.R
@@ -1,7 +1,17 @@
 # Define a custom function that returns draws from a Bernoulli rv
 
 my_rbernoulli <- function(n, p) { 
-  x <- runif(n, min = 0, max = 1)
+  x <- c()
+  y <- runif(n, min = 0, max = 1)
+  for (i in 1:n){
+    if (y[i] <= p){
+      #Use the knowledge in 9(a), use 1{U<=p} to simulate X
+      x <- c(x,1)
+    } else if (y[i] > p){
+      x <- c(x,0)
+    }
+  }
+  
   # Return draws
   return(x)
 }#MY_RBERNOULLI
@@ -10,3 +20,4 @@ my_rbernoulli <- function(n, p) {
 x <- my_rbernoulli(10000, 0.5)
 length(x) == 10000 # should return TRUE
 mean(x) # should a number near 0.5
+

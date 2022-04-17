9adeb72f8a8367f06e5993f6b1b6dbf2a8faed47
Haichuan23 <haichuanwang23@gmail.com>
2022-04-16 23:41:35 -0500

    HW25b new

--- HW2_Problem5b.R
+++ HW2_Problem5b.R
@@ -1,2 +1,17 @@
-df <- read.csv("data/ak91.csv")
+df <- read.csv("/Users/haichuan/ECON21020:Econometrics/ak91.csv")
+
+# Store years of education and the weekly wage in separate variables
+yrs_educ <- df$YRS_EDUC
+wkly_wage <- df$WKLY_WAGE
+
+sum <- 0
+count_college <- 0
+for (i in 1:length(yrs_educ)){
+  if (yrs_educ[i] == 16){
+    sum= sum+wkly_wage[i]
+    count_college = count_college+1
+  }
+}
+mucollege <- sum / count_college
+mucollege
 

9be1f99cdb9ce210bc8a87c4a0c48e568252feb9
Haichuan23 <haichuanwang23@gmail.com>
2022-04-17 00:28:54 -0500

    edit 5c

--- HW2_Problem5c.R
+++ HW2_Problem5c.R
@@ -3,8 +3,18 @@ df <- read.csv("/Users/haichuan/ECON21020:Econometrics/ak91.csv")
 yrs_educ <- df$YRS_EDUC
 wkly_wage <- df$WKLY_WAGE
 
+#part a , calculate p_x
+count <- 0
+for (val in yrs_educ){
+  if (val == 16) count = count + 1
+}
+
+n <- 329509
+p_x = count/n
+
+#part b, calculate mean wage
 sum <- 0 
-se_sum <- 0
+var <- 0
 count_college <- 0
 for (i in 1:length(yrs_educ)){
   if (yrs_educ[i] == 16){
@@ -16,9 +26,11 @@ mucollege <- sum / count_college # mean wage of college graduates
 
 for (i in 1:length(yrs_educ)){
   if(yrs_educ[i]==16){
-    se_sum = se_sum+ (wkly_wage[i]-mucollege)^2 
+    var = var + (wkly_wage[i]-mucollege)^2 
   }
 }
 
-se_college = se_sum/(count_college-1) #calculate the variance
+var = var/(count_college-1) #calculate the variance
+se_college = sqrt(var/p_x)/sqrt(count_college)
 se_college
+
